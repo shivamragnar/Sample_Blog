67024b4309a34547008e7918ffe6bb945a166877
shivam sharma <mail2shivam94@gmail.com>
2018-10-26 11:19:10 +0530

    sample blog starting of Routing

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, Link, NavLink, Switch } from 'react-router-dom';
+
+import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from '../Blog/FullPost/FullPost'
+
+class Blog extends Component {
+    render () {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink 
+                                    to="/" 
+                                    exact
+                                    activeClassName='my-active'
+                                    activeStyle={{
+                                        color : '#fa923f',
+                                        textDecoration:'underline'
+                                    }} >Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                        <Route path="/" exact component={Posts} />
+                        <Route path="/new-post" component={NewPost} />
+                        <Route path="/:id" exact component={FullPost} />
+                </Switch>
+               
+            </div>
+        ); 
+    }
+}
+
+export default Blog;
